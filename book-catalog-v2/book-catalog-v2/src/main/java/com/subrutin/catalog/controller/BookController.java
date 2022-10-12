344c36fb248e476a25b0dfaf5b03a981f23a3139
agung <hermawan.agung12@gmail.com>
2022-10-12 14:01:58 +0700

    add validation

--- book-catalog-v2/book-catalog-v2/src/main/java/com/subrutin/catalog/controller/BookController.java
+++ book-catalog-v2/book-catalog-v2/src/main/java/com/subrutin/catalog/controller/BookController.java
@@ -2,8 +2,12 @@ package com.subrutin.catalog.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -40,10 +44,18 @@ public class BookController {
     }
 
     @PostMapping("/new")
-    public String addNewBook(@ModelAttribute("bookCreateDTO") BookCreateDTO dto, Model model) 
+    public String addNewBook(@ModelAttribute("bookCreateDTO") @Valid BookCreateDTO dto, 
+        BindingResult bindingResult, Errors errors ,Model model) 
     {
+
+        //Jika error
+        if (errors.hasErrors()) {
+            model.addAttribute("bookCreateDTO", dto);    
+            return "book/book-new";       
+        }
         bookService.createNewBook(dto);
         return "redirect:/book/list";
+    
     }
 
 }
--- book-catalog-v2/book-catalog-v2/src/main/java/com/subrutin/catalog/dto/BookCreateDTO.java
+++ book-catalog-v2/book-catalog-v2/src/main/java/com/subrutin/catalog/dto/BookCreateDTO.java
@@ -1,11 +1,15 @@
 package com.subrutin.catalog.dto;
 
+import javax.validation.constraints.NotBlank;
+
 import lombok.Data;
 
 @Data
 public class BookCreateDTO {
     
+    @NotBlank
     private String bookTitle;
+    @NotBlank
     private String authorName;
     private String description;
 
